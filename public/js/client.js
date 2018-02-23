91cd48981575e8feec97386d9d8a133515ff29c8
Mukesh M <me@mukeshm.com>
2018-02-24 02:04:13 +0530

    Change event trigerring for editor content change

--- public/js/client.js
+++ public/js/client.js
@@ -3,7 +3,12 @@ let pusher = new Pusher('dd3d1cb1e89ac32335b4', {
   encrypted: true
 })
 
-let codeEditor = document.getElementById('code')
+let codeEditor = CodeMirror.fromTextArea(document.getElementById('code'), {
+  mode: 'javascript',
+  // readOnly: true, // non-editable
+  lineNumbers: true
+})
+
 let pathname = window.location.pathname
 let bufferid = 'private' + pathname.replace(/\//g, '-')
 let editEvent = 'client-text-edit'
@@ -11,11 +16,11 @@ let editEvent = 'client-text-edit'
 let channel = pusher.subscribe(bufferid)
 
 channel.bind(editEvent, function (data) {
-  codeEditor.value = data
+  codeEditor.setValue(data)
 })
 
 channel.bind('pusher:subscription_succeeded', function () {
-  codeEditor.addEventListener('input', changeHandler)
+  codeEditor.getInputField().addEventListener('input', changeHandler)
 })
 
 channel.bind('pusher:subscription_error', function (e) {
@@ -25,5 +30,13 @@ channel.bind('pusher:subscription_error', function (e) {
 
 // on input change
 function changeHandler (event) {
-  channel.trigger(editEvent, event.target.value)
+  channel.trigger(editEvent, codeEditor.getValue())
 }
+
+let peers = ["MUK3SH", "BH4V4NI", "V3NK3Y", "1NDU5"]
+let peer_ul = document.getElementById("peer-list")
+peers.forEach((item) => {
+  var li = document.createElement("li")
+  li.appendChild(document.createTextNode(item))
+  peer_ul.appendChild(li)
+})
